ef21d6b07d50b7601d3023582152c7fbfd39615f
Florian Stellbrink <flo@stellbr.ink>
2021-10-24 19:44:35 +0200

    Skip building material
    During build babylon expects a working gl context. That breaks with interesting materials.
    Update test material to one that triggers this problem.

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ import { LoaderDefinitionFunction } from 'webpack';
 const nodeMaterialLoader: LoaderDefinitionFunction = function(source) {
   const engine = new Engine(null);
   const scene = new Scene(engine);
-  const nodeMaterial = NodeMaterial.Parse(JSON.parse(source), scene);
+  const nodeMaterial = new NodeMaterial('', scene);
+  nodeMaterial.loadFromSerialization(JSON.parse(source));
   const generated = nodeMaterial.generateCode().split('\n');
 
   return [
